457b9ce295bb4a8db6fa73efb93309c066c399cf
MKS-Student <mks.student@hackreactor.com>
2015-03-04 16:57:35 -0800

    Added time complexities for LinkedList and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -54,4 +54,7 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .addToTail is O(1)
+ .removeHead is O(1)
+ .contains is O(n)
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -39,4 +39,6 @@ treeMethods.contains = function(target){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .addChild is O(1)
+ .contains is O(n)
  */
