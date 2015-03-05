d3f99b436cb7747986b1f6231c2d204523090daf
MKS-Student <mks.student@hackreactor.com>
2015-03-04 16:05:10 -0800

    Added semicolons to pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -10,7 +10,7 @@ var Stack = function() {
 Stack.prototype.push = function(value) {
 	this.storage[this.count] = value;
 	this.count++;
-}
+};
 
 Stack.prototype.pop = function() {
 	if (this.count > 0) {
@@ -19,10 +19,10 @@ Stack.prototype.pop = function() {
 		delete this.storage[this.count];
 		return temp;
 	}
-}
+};
 
 Stack.prototype.size = function() {
 	return this.count;
-}
+};
 
 
