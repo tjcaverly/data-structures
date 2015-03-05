9370aa9c791c9660948fade9ba10b7511ac26d02
MKS-Student <mks.student@hackreactor.com>
2015-03-05 11:45:43 -0800

    Updated hashTable to grow and shrink

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._filled = 0;
 
 };
 
@@ -11,13 +12,19 @@ HashTable.prototype.insert = function(k, v){
   } else {
   	this._storage.get(i).push([k,v]);
   }	
-
+  this._filled++;
+  if (this._filled >= .75 * this._limit) {
+    this._newLimit(this._limit * 2);
+  }
 
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var retrieved = this._storage.get(i);
+  if (!retrieved){
+    return null;
+  }
   for (var j=0; j<retrieved.length; j++) {
   	if (retrieved[j][0]===k) {
   		return retrieved[j][1];
@@ -29,12 +36,34 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
 	var i = getIndexBelowMaxForKey(k, this._limit);
 	var retrieved = this._storage.get(i);
+  if (!retrieved){
+    return;
+  }
   for (var j=0; j<retrieved.length; j++) {
   	if (retrieved[j][0]===k) {
   		retrieved.pop(j);
+      this._filled--;
   	}
   }
+  if (this._filled < .25 * this._limit && this._limit > 1) {
+    this._newLimit(Math.floor(this._limit/2));
+  }
+
 };
+
+HashTable.prototype._newLimit = function(limit) {
+  var hashTable = this;
+  var oldArray = this._storage;
+  this._storage = LimitedArray(limit);
+  this._filled = 0;
+  this._limit = limit;
+  oldArray.each(function(array){
+    _.each(array, function(keyValuePair){
+      hashTable.insert(keyValuePair[0], keyValuePair[1]);
+    });
+  });
+
+}
 
 
 
